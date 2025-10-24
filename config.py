73b0caee6752436759a6646ebd3ee53191f0f842
SofiaDrozhzhina <sofiadrozzina569@gmail.com>
2025-10-24 10:32:55 +0300

    config.py

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ load_dotenv()
 
 # Ожидается, что вы установите DATABASE_URL в .env или в окружении:
 # пример: postgresql://user:password@localhost:5432/mydb
-DATABASE_URI = os.getenv("DATABASE_URL", "postgresql+psycopg://postgres:Post/.77SD_/@localhost:5432/course_records_db")
+#DATABASE_URI = os.getenv("DATABASE_URL", "postgresql+psycopg://postgres:Post/.77SD_/@localhost:5432/course_records_db")
+DATABASE_URI = os.getenv("DATABASE_URL", "postgresql+psycopg://user_04:password4@10.115.0.67:5432/edu_practice01_04")
 
 # Flask settings
 SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
